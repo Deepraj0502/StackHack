6ab101e68e31e8ed8af154e9b51d1bc51981d8ad
Deepraj0502 <pagaredeepraj05@gmail.com>
2024-08-17 13:24:01 +0530

    first commit

---
+++ src/Components/Landing.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavbarComp from './NavbarComp'
+
+export default function Landing() {
+    return (
+        <div className='bg-[url("https://ik.imagekit.io/ok2wgebfs/Movie/2.png?updatedAt=1723877096116")] h-[100vh] w-[100vw] bg-cover brightness-75'>
+            <NavbarComp />
+            <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+                
+            </div>
+        </div>
+    )
+}
